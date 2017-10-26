5c2d1404ad9bb7adc24bbe615645bdcd0c60dd9a
Richard M. Salmon <rsalmon@sithempire.local>
2017-10-26 13:23:56 -0500

    Merge branch 'master' of https://github.com/ElectroMagus/ESP8266-Mesh

---
+++ holding/Software_Serial_ESP.ino
@@ -0,0 +1,78 @@
+#include <Adafruit_GPS.h>
+
+
+
+// Mesh Stuff
+#include "painlessMesh.h"
+#define   MESH_PREFIX     "MPan"
+#define   MESH_PASSWORD   "d0ntp33k"
+#define   MESH_PORT       6767
+painlessMesh  mesh;
+
+size_t logServerId = 0;
+
+// Send message to the logServer every 10 seconds
+Task myLoggingTask(10000, TASK_FOREVER, []() {
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& msg = jsonBuffer.createObject();
+    msg["topic"] = "sensor";
+    msg["value"] = random(0, 180);
+
+    String str;
+    msg.printTo(str);
+    if (logServerId == 0) // If we don't know the logServer yet
+        mesh.sendBroadcast(str);
+    else
+        mesh.sendSingle(logServerId, str);
+
+    // log to serial
+    msg.printTo(Serial);
+    Serial.printf("\n");
+});
+
+// End Mesh Stuff
+
+
+SoftwareSerial mySerial(14, 12); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Connected to debug port.");
+
+  // Mesh Stuff
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, STA_AP, AUTH_WPA2_PSK, 6 );
+  mesh.onReceive(&receivedCallback);
+    // Add the task to the mesh scheduler
+    mesh.scheduler.addTask(myLoggingTask);
+    myLoggingTask.enable();
+
+  mySerial.begin(9600);
+  Serial.println("Connected to GPS via Serial");
+
+
+}
+
+void loop() { // run over and over
+  mesh.update();
+}
+
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("logClient: Received from %u msg=%s\n", from, msg.c_str());
+
+  // Saving logServer
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(msg);
+  if (root.containsKey("topic")) {
+      if (String("logServer").equals(root["topic"].as<String>())) {
+          // check for on: true or false
+          logServerId = root["nodeId"];
+          Serial.printf("logServer detected!!!\n");
+      }
+      Serial.printf("Handled from %u msg=%s\n", from, msg.c_str());
+  }
+}
