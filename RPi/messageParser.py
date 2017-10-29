85c9d59486c01c8557221e7f3c5ab516244fdbb5
ElectroMagus <ricky@salmonfamily.net>
2017-10-29 01:49:12 +0000

    Added in first handler function and commented the crap out of new Python magic

--- RPi/messageParser.py
+++ RPi/messageParser.py
@@ -1,13 +1,19 @@
 filename = "parsedLogServer.messages"
-
 node_id = set()
 
+def main():
+    file = open(filename, "r")
+    for line in file:
+        rawMsg = line.split(' ')                # Whitespace separation for now 
+        sensorMsg = rawMsg[6:8]                 # The goodies are in columns 7 and 8
+        if sensorMsg[0] not in node_id:     # A set evaluation function - Python rocks...  
+            node_id.add(sensorMsg[0])       # Adds items to the set if they arn't there already
+        sendMQTT(sensorMsg[1])                  # Pass off the already formatted message to a handler
+    print len(node_id)                          # Num of unique Sensor ID's
 
-file = open(filename, "r")
-for line in file:
-    rawMsg = line.split(' ')
-    sensorMsg = rawMsg[6:8]
-    if sensorMsg[0] not in node_id: 
-        node_id.add(sensorMsg[0])
+
+
+def sendMQTT(msg):
+    print msg
     
-print len(node_id)
+main()
